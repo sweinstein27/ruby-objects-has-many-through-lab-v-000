406813637358a5760d0784cd3d09a76c2929ea75
Scott Weinstein <sweinstein27@gmail.com>
2018-05-23 14:54:43 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,7 +10,7 @@ def initialize(name)
   @name = name
   @@all << self
   @songs = []
-  @genre = []
+  @genres = []
 end
 
 def self.all
@@ -20,7 +20,7 @@ end
 def new_song(name, genre)
 song = Song.new(name, self, genre)
 @songs << song
-@genre << genre
+@genres << genre
 song
 end
 
