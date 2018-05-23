e14f1cf08a5d59dbcd6f173b9e3f8049dba6a5ce
Scott Weinstein <sweinstein27@gmail.com>
2018-05-23 18:06:23 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -15,7 +15,7 @@ class Doctor
     @@all
   end
 
-  def new_appointment(date, patient)
+  def new_appointment(patient, date)
     appointment = Appointment.new(patient, self, date)
     binding.pry
     @appointments << appointment
